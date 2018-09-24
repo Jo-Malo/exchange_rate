a4ff698631582913cdfcb9554ea15c359406d1c2
Jo Malo <juanitamalo.p@gmail.com>
2018-09-24 11:00:15 +0100

    updating changes

--- lib/exchange_rate.rb
+++ lib/exchange_rate.rb
@@ -1,17 +1,9 @@
-require_relative "extra/save_data"
-require_relative "extra/error"
-require_relative "extra/conversion"
+require_relative "helper/save_data"
+require_relative "helper/error"
+require_relative "helper/conversion"
 require "open-uri"
 
 module ExchangeRate
-
-  def self.create_store(file_path)
-    SaveData.instance.create_store(file_path)
-  end
-
-  def self.set_data_source(new_datasource, new_xmlns)
-    SaveData.instance.set_data_source(new_datasource, new_xmlns)
-  end
 
   def self.at(today = Date.today, from_currency, to_currency)
     return Conversion.get_rate_at_date(today, from_currency, to_currency)
